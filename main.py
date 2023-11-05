ea550b029d3df6e56d92dac538343be64ecea82b
Anish Karthik <89824626+anishfish2@users.noreply.github.com>
2023-11-04 23:00:24 -0500

    Added gpt suggestions

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 from flask_cors import CORS
 import pandas as pd
 import json
+import openai
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -34,7 +37,7 @@ def analyze():
     #Determine approval
     def add_filter(df):
         approved_list = []
-        lti_list = []
+        ltv_list = []
         credit_score_list = []
         dti_43_list = []
         dti_36_list = []
@@ -43,13 +46,13 @@ def analyze():
         for index, row in df.iterrows():
             approved = 'Y'
             note = []
-            credit_score, lti, dti_43, dti_36, fedti = 0, 0, 0, 0, 0
+            credit_score, ltv, dti_43, dti_36, fedti = 0, 0, 0, 0, 0
 
             if row['credit_score'] < 640:
                 approved = 'N'
                 credit_score = 1
             if row['LTV'] >= .8:
-                lti = 1
+                ltv = 1
             if row['DTI'] >= .43:
                 approved = 'N'
                 dti_43 = 1
@@ -62,14 +65,14 @@ def analyze():
 
             approved_list.append(approved)
             credit_score_list.append(credit_score)
-            lti_list.append(lti)
+            ltv_list.append(ltv)
             dti_43_list.append(dti_43)
             dti_36_list.append(dti_36)
             fedti_list.append(fedti)
 
         df['approved'] = approved_list
         df['credit_score_check'] = credit_score_list
-        df['lti_check'] = lti_list
+        df['ltv_check'] = ltv_list
         df['dti_43_check'] = dti_43_list
         df['dti_36_check'] = dti_36_list
         df['fedti_check'] = fedti_list
@@ -86,13 +89,52 @@ def return_averages():
         data = json.load(file)
     return data
 
-@app.route("/percent_rejected_by_factor")
+@app.route("/percent-rejected-by-factor")
 def return_percent_by_factor():
     return (
-        {"credit_score_check":4028/8400,
-            "lti_check": 6258/8400,
+        {
+            "credit_score_check":4028/8400,
+            "ltv_check": 6258/8400,
             "dti_43_check": 6526/8400,
             "dti_36_check": 1380/8400,
             "fedti_check": 6286/8400
         }
     )
+
+@app.route("/get-suggestion")
+def ask_gpt():
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_KEY')
+
+    sent = request.get_json()['factors']
+    sent_merged = ""
+    prompt = "A user has just used an app to determine whether they are eligible for a loan."
+
+    for i in sent:
+        sent_merged += i
+
+    if "credit" in sent_merged:
+        prompt += "Currently, their credit score is below 640."
+    if "ltv" in sent_merged:
+        prompt += "Currently, their LTV is greater or equal to 80%."
+    if "dti_43" in sent_merged:
+        prompt += "Currently, their DTI is greater than 43%."
+    if "dti_36" in sent_merged:
+        prompt += "Currently, their DTI is greater than 36 percent but less than 43%."
+    if "fedt" in sent_merged:
+        prompt += "Currently, their FEDTI is greater than 28%."
+
+    
+    prompt += "Can you reccomend some advice, tips, and suggestions as to what they might be able to change in order to get the loan approved?"
+
+    print(prompt)
+
+    response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages= [{"role": "user", "content": prompt}]
+    )
+
+    total = response['choices'][0]['message']['content']
+    response_text = total.split("\n")[0]
+
+    return response_text
